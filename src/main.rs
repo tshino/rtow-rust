20a77c07f2393cd8a0cace711050d96d8028a5a3
tshino <tshino.ssr@gmail.com>
2020-06-08 00:20:01 +0900

    6.7 Common Constants and Utility Functions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod vec3;
 mod ray;
 mod hittable;
-mod sphere;
 mod hittable_list;
+mod sphere;
 
 use vec3::Vec3;
 use vec3::Point3;
 use vec3::Color;
 use ray::Ray;
+use hittable::Hittable;
+use hittable::HitResult;
+use hittable_list::HittableList;
+use sphere::Sphere;
 
 pub fn print_color(pixel_color: Color) {
     let ir = (pixel_color.x * 255.0).round();
@@ -16,24 +20,9 @@ pub fn print_color(pixel_color: Color) {
     println!("{} {} {}", ir, ig, ib);
 }
 
-fn hit_sphere(center: &Point3, radius: f32, r: &Ray) -> f32 {
-    let oc = r.origin - *center;
-    let a = r.direction.length_squared();
-    let half_b = vec3::dot(oc, r.direction);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-half_b - discriminant.sqrt()) / a
-    }
-}
-
-fn ray_color(r: &Ray) -> Color {
-    let t =hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
-    if t > 0.0 {
-        let n = vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
-        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
+fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
+    if let HitResult::Hit(rec) = world.hit(r, 0.0, f32::INFINITY) {
+        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
     }
     let unit_direction = vec3::unit_vector(r.direction);
     let t = 0.5 * (unit_direction.y + 1.0);
@@ -55,13 +44,18 @@ fn main() {
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
     let vertical = Vec3::new(0.0, viewport_height, 0.0);
     let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+
     for j in (0..H).rev() {
         eprint!("\rScanlines remaing: {} ", j);
         for i in 0..W {
             let u = (i as f32) / ((W - 1) as f32);
             let v = (j as f32) / ((H - 1) as f32);
             let r = Ray::new(&origin, &(lower_left_corner + u * horizontal + v * vertical - origin));
-            let pixel_color = ray_color(&r);
+            let pixel_color = ray_color(&r, &world);
             print_color(pixel_color);
         }
     }
