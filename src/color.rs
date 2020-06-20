086b50437aac9382945887279c64269763612615
tshino <tshino.ssr@gmail.com>
2020-06-20 15:49:48 +0900

    8.3 Using Gamma Correction for Accurate Color Intensity

--- src/color.rs
+++ src/color.rs
@@ -2,14 +2,13 @@ use super::util;
 use super::vec3::Color;
 
 pub fn print_color(pixel_color: Color, samples_per_pixel: i32) {
-    let r = pixel_color.x;
-    let g = pixel_color.y;
-    let b = pixel_color.z;
+    let scale = (255.0 * 255.0) / samples_per_pixel as f32;
+    let r = (pixel_color.x * scale).sqrt();
+    let g = (pixel_color.y * scale).sqrt();
+    let b = (pixel_color.z * scale).sqrt();
 
-    let scale = 255.0 / samples_per_pixel as f32;
-
-    let ir = util::clamp(r * scale, 0.0, 255.0).round();
-    let ig = util::clamp(g * scale, 0.0, 255.0).round();
-    let ib = util::clamp(b * scale, 0.0, 255.0).round();
+    let ir = util::clamp(r, 0.0, 255.0).round();
+    let ig = util::clamp(g, 0.0, 255.0).round();
+    let ib = util::clamp(b, 0.0, 255.0).round();
     println!("{} {} {}", ir, ig, ib);
 }
